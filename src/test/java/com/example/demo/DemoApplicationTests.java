61ce4d669558a5b3c028e77ff0735163bb5ec9ff
[Bendevet] <[bnwogbo@mum.edu]>
2020-04-30 17:27:04 -0500

    test7

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+(
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+	classes = DemoApplication.class
+)
+@TestPropertySource(
+	locations = "classpath:application-test.properties"
+)
 class DemoApplicationTests {
 
 	@Test
